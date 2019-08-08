1cf98e3130df7dfba0a354170910910e4a3d00d3
py-hub <swathikolla@gmail.com>
2019-08-09 03:50:43 +0530

    added project files

---
+++ altran_test.py
@@ -0,0 +1,30 @@
+# Using urllib so we don't have to install requests library
+from urllib.request import Request, urlopen
+from urllib.error import URLError, HTTPError
+import json
+
+# Input an IP address
+search = input("Enter the search param (IP address e.g. '44:38:39:ff:ef:57'): ")
+URL = "https://api.macaddress.io/v1?"
+PARAMS = {"apiKey": "at_Km5ZQXTnfRsg42KBdeafhBpCdRuUP",
+          "output": "json",
+          "search": search}
+
+url = URL + '&'.join([k + '=' + v for k, v in PARAMS.items()])
+
+req = Request(url)
+try:
+    response = urlopen(req)
+except HTTPError as e:
+    print("The server couldn't fulfill the request.")
+    print('Error code: ', e.code)
+    print('Error: ', e.read())
+except URLError as e:
+    print('We failed to reach a server.')
+    print('Reason: ', e.reason)
+else:
+    response = json.loads(response.read())
+    company_name = response['vendorDetails']['companyName']
+    company_address = response['vendorDetails']['companyAddress']
+    print("Company Name: ", '\033[94m' + company_name + '\033[0m')
+    print("Company Address: ", '\033[94m' + company_address + '\033[0m')
